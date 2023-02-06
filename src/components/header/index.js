0a6ede9f21fd82773062843f987ab54f197b9685
Сергей Сироткин <sergeysirotkin@MacBook-Air-Sergej.local>
2023-02-06 19:05:46 +0200

    Added hiding behavior for header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,45 +1,55 @@
 import React from "react"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 
+import { useScrollDirection } from "../../hooks/useScrollDirection"
 import logo from "../../images/logo.svg"
 import StyledButton from "../styledButton"
 
 import { HeaderContainer, HeaderLogo, Navlist, NavlistItem } from "./styled"
 
-const listItems = [
-  { name: "About", anchor: "#footer" },
-  { name: "Experience", anchor: "#footer" },
-  { name: "Work", anchor: "#footer" },
-  { name: "Contact", anchor: "#footer" },
-]
+const Header = () => {
+  const { isTop, scrollDirection } = useScrollDirection()
 
-const Header = () => (
-  <HeaderContainer>
-    <HeaderLogo src={logo} alt="logo" />
+  const isScrollDown = scrollDirection === "down"
 
-    <nav>
-      <Navlist>
-        {listItems.map(({ name, anchor }, index) => (
-          <NavlistItem key={index}>
-            <AnchorLink to={anchor}>
-              <span>0{index + 1}.</span>
-              {name}
-            </AnchorLink>
-          </NavlistItem>
-        ))}
+  console.log(isTop)
+  console.log(scrollDirection)
 
-        <StyledButton fontSize="13px">
-          <a
-            href="https://brittanychiang.com/resume.pdf"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Resume
-          </a>
-        </StyledButton>
-      </Navlist>
-    </nav>
-  </HeaderContainer>
-)
+  const listItems = [
+    { name: "About", anchor: "#footer" },
+    { name: "Experience", anchor: "#footer" },
+    { name: "Work", anchor: "#footer" },
+    { name: "Contact", anchor: "#footer" },
+  ]
+
+  return (
+    <HeaderContainer isTop={isTop} isScrollDown={isScrollDown}>
+      <HeaderLogo src={logo} alt="logo" />
+
+      <nav>
+        <Navlist>
+          {listItems.map(({ name, anchor }, index) => (
+            <NavlistItem key={index}>
+              <AnchorLink to={anchor}>
+                <span>0{index + 1}.</span>
+                {name}
+              </AnchorLink>
+            </NavlistItem>
+          ))}
+
+          <StyledButton fontSize="13px">
+            <a
+              href="https://brittanychiang.com/resume.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
+          </StyledButton>
+        </Navlist>
+      </nav>
+    </HeaderContainer>
+  )
+}
 
 export default Header
--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -11,6 +11,10 @@ export const HeaderContainer = styled.div`
   z-index: 100;
   background-color: rgba(10, 25, 47, 0.85);
   padding: 0px 50px;
+  backdrop-filter: blur(10px);
+  transition: box-shadow 0.2s ease, top 0.4s ease 0.2s;
+  box-shadow: ${props => !props.isTop && "0 10px 30px -10px rgba(2,12,27,0.7)"};
+  top: ${props => (props.isScrollDown ? "-100px" : 0)};
 `
 
 export const HeaderLogo = styled.img`
