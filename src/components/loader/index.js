736ac573df5e864152ef85d5292f0515b4e333e3
Sergey Sirotkin <sergey.sirotkin@nuevacode.com>
2023-03-29 12:51:35 +0300

    Loader component created - without animation

---
+++ src/components/loader/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Logo from "../../images/logo.svg"
+
+const Loader = () => {
+  return <Logo />
+}
+
+export default Loader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,38 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import Layout from "../components/layout"
+import Loader from "../components/loader"
 import Seo from "../components/seo"
 import HomePage from "../containers/HomePage"
 
-const IndexPage = () => (
-  <Layout>
-    <HomePage />
-  </Layout>
-)
+const IndexPage = () => {
+  const [showLoader, setShowLoader] = useState(true)
+
+  useEffect(() => {
+    const isVisitedBefore = localStorage.getItem("visitedBefore")
+
+    if (isVisitedBefore) {
+      setShowLoader(false)
+    } else {
+      localStorage.setItem("visitedBefore", true)
+      setTimeout(() => {
+        setShowLoader(false)
+      }, 4000)
+    }
+  }, [])
+
+  return (
+    <>
+      {showLoader ? (
+        <Loader />
+      ) : (
+        <Layout>
+          <HomePage />
+        </Layout>
+      )}
+    </>
+  )
+}
 
 export const Head = () => <Seo title="Home" />
 
